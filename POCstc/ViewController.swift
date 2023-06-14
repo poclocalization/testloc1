2fbc380431413ba6a59b2b39279f7ca6fb984090
Noura Alhoshan <Nouraalhoshan1@hotmail.com>
2023-06-14 13:27:51 +0300

    Initial Commit

---
+++ POCstc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCstc
+//
+//  Created by Noura Alhoshan on 14/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
