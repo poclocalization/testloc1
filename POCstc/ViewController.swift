266b49ce57bfc1de93b6f0457b2ea5fb486fd353
Noura Alhoshan <nooni-4321@hotmail.com>
2023-06-14 14:27:01 +0300

    Update ViewController.swift

--- POCstc/ViewController.swift
+++ POCstc/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-                  self.Hello.text = "its test"
+                  self.Hello.text = "its test123"
     }
     @IBOutlet weak var Hello: UILabel!
 
